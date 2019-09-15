5c3dd48c7bc3db0c83ca028c7d5d5d3b1c4f86fe
Christian Falch <christian.falch@gmail.com>
2019-09-15 12:14:10 +0200

    Updated with correct support for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    '@react-native-community', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react-hooks'],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  rules: {
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+  },
 };
