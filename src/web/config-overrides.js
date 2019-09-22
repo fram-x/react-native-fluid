56181f5a58d09cc09ff0274dc9750fde1e762ba7
Christian Falch <christian.falch@gmail.com>
2019-09-22 21:29:55 +0200

    Updated with working Fluid.View for web

--- src/web/config-overrides.js
+++ src/web/config-overrides.js
@@ -2,14 +2,6 @@
 const webpack = require("webpack");
 
 module.exports = function override(config, env) {
-  config.resolve = {
-    ...config.resolve,
-    alias: {
-      ...config.resolve.alias,
-      "react-native-fluid-animations": "../packages/animated",
-      "react-native-fluid-transitions": "../packages/transitions",
-    },
-  };
   config.plugins.push(
     new webpack.DefinePlugin({
       __DEV__: true,
--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -1,12 +1,30 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { AnimationProvider } from "react-native-fluid-animations";
+import React, { useState, useCallback } from "react";
+import { StyleSheet, Text } from "react-native";
+import Fluid from "react-native-fluid-transitions";
 
+const styles = {
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 30,
+  },
+  active: {
+    transform: [{ rotate: "90deg" }],
+  },
+  inactive: {
+    transform: [{ rotate: "0deg" }],
+  },
+};
 const App = () => {
+  const [active, setActive] = useState(false);
+  const toggle = useCallback(() => setActive(a => !a), [active]);
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <Fluid.View staticStyle={styles.container} onPress={toggle}>
+      <Fluid.View style={active ? styles.active : styles.inactive}>
+        <Text>Hello World</Text>
+      </Fluid.View>
+    </Fluid.View>
   );
 };
 
