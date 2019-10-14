8de42be979ed7461cf517a65bb23485f6519e762
Christian Falch <christian.falch@gmail.com>
2019-10-14 09:00:03 +0200

    Adjusted likeheart

--- src/examples/Interactions/LikeHeart.tsx
+++ src/examples/Interactions/LikeHeart.tsx
@@ -1,23 +1,24 @@
-import React, {useState} from 'react';
-import {StyleSheet, View, TextStyle} from 'react-native';
-import Fluid, {createFluidComponent} from 'react-native-fluid-transitions';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {IconProps} from 'react-native-vector-icons/Icon';
-import {ColorA} from '../colors';
+import React, { useState } from "react";
+import { StyleSheet, View, TextStyle } from "react-native";
+import Fluid, { createFluidComponent } from "react-native-fluid-transitions";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { IconProps } from "react-native-vector-icons/Icon";
+import { ColorA } from "../colors";
 
 const FluidIcon = createFluidComponent<IconProps, TextStyle>(Icon, false);
 
 const LikeHeart = () => {
   const [likes, setLikes] = useState(0);
-  const state = {name: 'likes', active: true, value: likes};
+  const state = { name: "likes", active: true, value: likes };
   const onLike = () => setLikes(l => l + 1);
   const config = Fluid.createConfig({
     onEnter: {
-      state: 'likes',
+      state: "likes",
       interpolation: {
-        styleKey: 'transform.scale',
+        styleKey: "transform.scale",
         outputRange: [1, 1.5, 1],
       },
+      animation: Fluid.Animations.Springs.Gentle,
     },
   });
   return (
@@ -36,9 +37,12 @@ const LikeHeart = () => {
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     width: 44,
     height: 44,
   },
 });
 
-export {LikeHeart};
+export { LikeHeart };
