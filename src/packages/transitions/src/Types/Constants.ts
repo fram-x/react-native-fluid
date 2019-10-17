a63652f4c61948eb8099152128ca1cdfe0ec16db
Christian Falch <christian@framx.no>
2019-10-17 11:06:22 +0200

    Removed static and root interpolator

--- src/packages/transitions/src/Types/Constants.ts
+++ src/packages/transitions/src/Types/Constants.ts
@@ -1,9 +1,6 @@
 export const StateMounted = "mounted";
 export const StateUnmounted = "unmounted";
 
-export const InterpolatorStatic = "__interpolatorStatic";
-export const InterpolatorLabelRoot = "__root";
-
 export const DefaultStaggerMs = 100;
 
 export const AsGroup = -1001;
