ac8c6bb313764f6873551a9d70f327da16f74bde
Christian Falch <christian.falch@gmail.com>
2019-11-07 08:15:58 +0100

    Trying to fix normalizeColor

--- src/packages/animated/src/react-native-animated/Implementation/getProcessedColor.ts
+++ src/packages/animated/src/react-native-animated/Implementation/getProcessedColor.ts
@@ -2,10 +2,10 @@ import { IAnimationNode } from "../../Types";
 import { isAnimatedNode } from "./utils";
 import { Animated } from "react-native";
 
-const normalizeColor = require("normalizeColor");
+import { normalizeColor } from "./normalizeColor";
 
 export const getProcessedColor = (
-  value: string | number | IAnimationNode
+  value: string | number | IAnimationNode,
 ): number => {
   if (isAnimatedNode(value)) {
     // @ts-ignore
